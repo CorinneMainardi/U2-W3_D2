51f2ca9458ceaced83320632709e640319145d24
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-09-24 16:51:57 +0200

    esercizio1

---
+++ script.js
@@ -0,0 +1,26 @@
+const localStorageKey = "registration-memory";
+
+const form = document.querySelector("form");
+const input = document.getElementById("name");
+
+const handleSubmit = function (e) {
+  e.preventDefault();
+
+  const name = input.value;
+  console.log("nome indicato", name);
+  let nameInLocaleStorage = logalStorage.getItem(localStorageKey);
+  if (!nameInLocaleStorage) {
+    nameInLocaleStorage = [];
+  } else {
+    nameInLocaleStorage = JSON.parse(nameInLocaleStorage);
+  }
+  if (name) {
+    nameInLocaleStorage.push(name);
+  } else {
+    alert("non hai inserito un nome, inseriscilo!");
+  }
+  if (nameInLocaleStorage.length > 1) {
+    input.setAttribute("placeholder", `${nameInLocaleStorage[nameInLocaleStorage.length - 1]}`);
+  }
+};
+form.addEventListener("submit", handleSubmit);
